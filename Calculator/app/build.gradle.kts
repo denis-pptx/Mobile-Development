bf2f065175d4399a0db2b5e4ef78f6bd16bbd43e
Denis-HP <denis.pptx@gmail.com>
2024-03-14 13:28:46 +0300

    styles for optional operations

--- Calculator/app/build.gradle.kts
+++ Calculator/app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "edu.deniskonchik.calculator"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
